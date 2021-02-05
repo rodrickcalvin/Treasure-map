ec0687e7f8bfe6cff4c2c2f58cb7587089d91154
rodrickcalvin <rodrickwamala@gmail.com>
2021-02-05 17:11:54 +0300

    adds title name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'topcoder-learning-angular';
+  title = 'Topcoder Angular Level 1';
 }
